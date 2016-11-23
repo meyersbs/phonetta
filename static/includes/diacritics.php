667eb195f3b531f199c04bc2db8ad6b12fc929a5
Benjamin Meyers <ben@lionlogic.org>
2016-11-22 23:19:15 -0500

    Added keyboards for non-phoneme IPA symbols.

---
+++ static/includes/diacritics.php
@@ -0,0 +1,85 @@
+<?php ?>
+<div class="row">
+	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 section-title pull-left">
+		&nbsp;&nbsp;&nbsp;diacritics
+	</div>
+</div>
+<div class="row">
+	<div id="dia1" title="voiceless (below consonants)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important; margin-left: 17px !important;" data-unicode=&#x325;>&#x25CC;&#x325;</div>
+	<div class="popover-content hide"><span>&#x6E;&#x325;</span><br><br>shortcut: alt+AAA<br>HTML: &amp;#x325;<br>UTF-16: 0x0325</div>
+	<div id="dia2" title="voiceless (above consonants)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x30A;>&#x25CC;&#x30A;</div>
+	<div class="popover-content hide"><span>&#x14B;&#x30A;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x30A;<br>UTF-16: 0x030A</div>
+	<div id="dia3" title="voiced (below consonants)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x32C;>&#x25CC;&#x32C;</div>
+	<div class="popover-content hide"><span>&#x73;&#x32C;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x32C;<br>UTF-16: 0x032C</div>
+	<div id="dia4" title="voiced (above consonants)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x30C;>&#x25CC;&#x30C;</div>
+	<div class="popover-content hide"><span>&#x14B;&#x30C;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x30C;<br>UTF-16: 0x030C</div>
+	<div id="dia5" title="more rounded" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x339;>&#x25CC;&#x339;</div>
+	<div class="popover-content hide"><span>&#x254;&#x339;</span><br><br>shortcut: alt+AAA<br>HTML: &amp;#x339;<br>UTF-16: 0x0339</div>
+	<div id="dia6" title="less rounded" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x31C;>&#x25CC;&#x31C;</div>
+	<div class="popover-content hide"><span>&#x254;&#x31C;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x31C;<br>UTF-16: 0x031C</div>
+	<div id="dia7" title="advanced" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x31F;>&#x25CC;&#x31F;</div>
+	<div class="popover-content hide"><span>&#x75;&#x31F;</span><br><br>shortcut: alt+AAA<br>HTML: &amp;#x31F;<br>UTF-16: 0x031F</div>
+	<div id="dia8" title="retracted" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x320;>&#x25CC;&#x320;</div>
+	<div class="popover-content hide"><span>&#x6E;&#x320;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x320;<br>UTF-16: 0x0320</div>
+	<div id="dia9" title="rhoticity" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x2DE;>&#x2DE;</div>
+	<div class="popover-content hide"><span>&#x25A;&nbsp;&#x61;&#x2DE;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2DE;<br>UTF-16: 0x02DE</div>
+</div>
+<div class="row">
+	<div id="dia10" title="centralized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important; margin-left: 17px !important;" data-unicode=&#x308;>&#x25CC;&#x308;</div>
+	<div class="popover-content hide"><span>&#x6E;&#x308;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x308;<br>UTF-16: 0x0308</div>
+	<div id="dia11" title="mid-centralized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x33D;>&#x25CC;&#x33D;</div>
+	<div class="popover-content hide"><span>&#x6E;&#x33D;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x33D;<br>UTF-16: 0x033D</div>
+	<div id="dia12" title="syllabic (below consonants)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x329;>&#x25CC;&#x329;</div>
+	<div class="popover-content hide"><span>&#x6E;&#x329;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x329;<br>UTF-16: 0x0329</div>
+	<div id="dia13" title="syllabic (above consonants)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x30D;>&#x25CC;&#x30D;</div>
+	<div class="popover-content hide"><span>&#x14B;&#x30D;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x30D;<br>UTF-16: 0x030D</div>
+	<div id="dia14" title="non-syllabic (below vowels)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x32F;>&#x25CC;&#x32F;</div>
+	<div class="popover-content hide"><span>&#x65;&#x32F;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x32F;<br>UTF-16: 0x032F</div>
+	<div id="dia15" title="non-syllabic (above vowels)" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x311;>&#x25CC;&#x311;</div>
+	<div class="popover-content hide"><span>&#x65;&#x311;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x311;<br>UTF-16: 0x0311</div>
+	<div id="dia16" title="breathy voiced" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x324;>&#x25CC;&#x324;</div>
+	<div class="popover-content hide"><span>&#x62;&#x324;&nbsp;&#x61;&#x324;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x324;<br>UTF-16: 0x0324</div>
+	<div id="dia17" title="creaky voiced" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x330;>&#x25CC;&#x330;</div>
+	<div class="popover-content hide"><span>&#x62;&#x330;&nbsp;&#x61;&#x330;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x330;<br>UTF-16: 0x0330</div>
+	<div id="dia18" title="velarized/pharyngealized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x334;>&#x25CC;&#x334;</div>
+	<div class="popover-content hide"><span>&#x26B;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x334;<br>UTF-16: 0x0334</div>
+</div>
+<div class="row">
+	<div id="dia19" title="raised" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important; margin-left: 17px !important;" data-unicode=&#x31D;>&#x25CC;&#x31D;</div>
+	<div class="popover-content hide"><span>&#x65;&#x31D;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x31D;<br>UTF-16: 0x031D</div>
+	<div id="dia20" title="lowered" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x31E;>&#x25CC;&#x31E;</div>
+	<div class="popover-content hide"><span>&#x65;&#x31E;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x31E;<br>UTF-16: 0x031E</div>
+	<div id="dia21" title="advanced tongue root" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x318;>&#x25CC;&#x318;</div>
+	<div class="popover-content hide"><span>&#x65;&#x318;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x318;<br>UTF-16: 0x0318</div>
+	<div id="dia22" title="retracted tongue root" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x319;>&#x25CC;&#x319;</div>
+	<div class="popover-content hide"><span>&#x65;&#x319;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x319;<br>UTF-16: 0x0319</div>
+	<div id="dia23" title="dental" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x32A;>&#x25CC;&#x32A;</div>
+	<div class="popover-content hide"><span>&#x74;&#x32A;&nbsp;&#x64;&#x32A;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x32A;<br>UTF-16: 0x032A</div>
+	<div id="dia24" title="apical" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x33A;>&#x25CC;&#x33A;</div>
+	<div class="popover-content hide"><span>&#x74;&#x33A;&nbsp;&#x64;&#x33A;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x33A;<br>UTF-16: 0x033A</div>
+	<div id="dia25" title="laminal" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x33B;>&#x25CC;&#x33B;</div>
+	<div class="popover-content hide"><span>&#x74;&#x33B;&nbsp;&#x64;&#x33B;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x33B;<br>UTF-16: 0x033B</div>
+	<div id="dia26" title="linguolabial" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x33C;>&#x25CC;&#x33C;</div>
+	<div class="popover-content hide"><span>&#x64;&#x33C;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x33C;<br>UTF-16: 0x033C</div>
+	<div id="dia27" title="nasalized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 44px !important;" data-unicode=&#x303;>&#x25CC;&#x303;</div>
+	<div class="popover-content hide"><span>&#x65;&#x303;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x303;<br>UTF-16: 0x0303</div>
+</div>
+<div class="row">
+	<div id="dia28" title="aspirated" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 49px !important; margin-left: 17px !important;" data-unicode=&#x2B0;>&#x25CC;&#x2B0;</div>
+	<div class="popover-content hide"><span>&#x74;&#x2B0;&nbsp;&#x64;&#x2B0;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2B0;<br>UTF-16: 0x02B0</div>
+	<div id="dia29" title="labialized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 50px !important;" data-unicode=&#x2B7;>&#x25CC;&#x2B7;</div>
+	<div class="popover-content hide"><span>&#x74;&#x2B7;&nbsp;&#x64;&#x2B7;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2B7;<br>UTF-16: 0x02B7</div>
+	<div id="dia30" title="palatalized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 50px !important;" data-unicode=&#x2B2;>&#x25CC;&#x2B2;</div>
+	<div class="popover-content hide"><span>&#x74;&#x2B2;&nbsp;&#x64;&#x2B2;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2B2;<br>UTF-16: 0x02B2</div>
+	<div id="dia31" title="velarized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 50px !important;" data-unicode=&#x2E0;>&#x25CC;&#x2E0;</div>
+	<div class="popover-content hide"><span>&#x74;&#x2E0;&nbsp;&#x64;&#x2E0;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2E0;<br>UTF-16: 0x02E0</div>
+	<div id="dia32" title="pharyngealized" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 50px !important;" data-unicode=&#x2E4;>&#x25CC;&#x2E4;</div>
+	<div class="popover-content hide"><span>&#x74;&#x2E4;&nbsp;&#x64;&#x2E4;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2E4;<br>UTF-16: 0x02E4</div>
+	<div id="dia33" title="nasal release" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 50px !important;" data-unicode=&#x207F;>&#x25CC;&#x207F;</div>
+	<div class="popover-content hide"><span>&#x64;&#x207F;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x207F;<br>UTF-16: 0x207F</div>
+	<div id="dia34" title="lateral release" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-md" style="width: 50px !important;" data-unicode=&#x2E1;>&#x25CC;&#x2E1;</div>
+	<div class="popover-content hide"><span>&#x64;&#x2E1;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x2E1;<br>UTF-16: 0x02E1</div>
+	<div id="dia35" title="no audible release" class="col-xs-1 col-sm-1 col-md-1 col-lg-1 btn btn-default well well-sm keyboard-button-lg" style="width: 49px !important;" data-unicode=&#x31A;>&#x25CC;&#x31A;</div>
+	<div class="popover-content hide"><span>&#x64;&#x31A;</span><br><br>shortcut: alt+AAAA<br>HTML: &amp;#x31A;<br>UTF-16: 0x031A</div>
+</div>
+
