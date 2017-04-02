88260b07f59f444019d952d69f67b9e5a723edca
Benjamin Meyers <meyersbs@users.noreply.github.com>
2017-04-01 20:13:40 -0400

    Update footer.php

--- static/includes/footer.php
+++ static/includes/footer.php
@@ -3,6 +3,6 @@
 ?>
 <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
 	<div id="footer" class="container">
-		Copyright <i class="fa fa-copyright"></i> 2016 <a href="mailto:ben@splat-library.org" target="_blank">Benjamin S. Meyers</a>
+		Copyright 2016-2017 <a href="mailto:ben@splat-library.org" target="_blank">Benjamin S. Meyers</a>
 	</div>
 </nav>
