65b12eba9d99c73d8103bc5afd9b6aefe7baca13
Benjamin S. Meyers <ben@lionlogic.org>
2019-08-15 12:04:47 -0400

    PHP has been purged!

--- static/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-?>
-<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-	<div id="footer" class="container">
-		Copyright 2016-2017 <a href="mailto:ben@splat-library.org" target="_blank">Benjamin S. Meyers</a>
-	</div>
-</nav>
